868ee4643dccd8aa5bdbed604a195c5199c601e5
Bhavesh <Bhaveshsp8624@gmail.com>
2022-04-25 15:31:08 -0400

    Started Pass Project Created Project Added Splash Screen Activity Added SignUp Screen Activity

---
+++ app/src/main/java/com/udaghoshwelfarengo/pass/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.udaghoshwelfarengo.pass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        goToNextActivity()
+    }
+    private fun goToNextActivity(){
+//        startActivity(Inte)
+    }
+}
